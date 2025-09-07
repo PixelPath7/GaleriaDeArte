f60002ede342f9457dc9a8aa98fba79fee2e74ad
ErnestoHrz <xernestohrz@gmail.com>
2025-09-07 20:02:25 -0300

    Segundo

--- GaleriaDeArte/core/views.py
+++ GaleriaDeArte/core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request,"core/index.html")
+
+def album(request):
+    return render(request,"core/album.html")
+
+def info(request):
+    return render(request,"core/info.html")
+
+def preguntas(request):
+    return render(request,"core/preguntas.html")
